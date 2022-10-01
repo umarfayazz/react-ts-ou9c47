90ed5eda377b72cd1d3e37e1fe70b8900c3deca3
umarfayazz <72308400+umarfayazz@users.noreply.github.com>
2022-10-01 07:45:39 +0500

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react';
 import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      monsters : []
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(Response=> Response.json)
+    .then(data => {
+      this.setState({
+        monsters: data.name
+      })
+    })
+  }
+  render(){
+
+    return (
+      <div>
+        <h1>{this.state.monsters.map(m=>{
+          
+        })}</h1>
+      </div>
+    );
+  }
+  }
