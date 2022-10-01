bde622edab835f08b9b5bce5761dd14678879b7a
umarfayazz <72308400+umarfayazz@users.noreply.github.com>
2022-10-01 09:01:51 +0500

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,34 +2,44 @@ import * as React from 'react';
 import './style.css';
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      monsters : []
-    }
+      monsters: [],
+    };
   }
 
-  componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(Response=> Response.json())
-    .then(data => {
-      this.setState(()=>{
-        return {monsters:data}
-      }),
-      ()=>{
-        console.log(this.state)
-      }
-    })
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((Response) => Response.json())
+      .then((data) => {
+        this.setState(() => {
+          return { monsters: data };
+        });
+      });
   }
-  render(){
-    
+  render() {
     return (
       <div>
-        
-        <div>{this.state.monsters.map(e=>{
-          return <h1 key={e.id}> {e.name} </h1>
-        })}</div>
+        <input
+          type="search"
+          placeholder="SEARCH MONSTER.."
+          onChange={(event) => {
+            const searchFiled = event.target.value.toLocaleLowerCase();
+            const filteredArray = this.state.monsters.filter((user) => {
+              return user.name.toLocaleLowerCase().includes(searchFiled);
+            });
+            this.setState(() => {
+              return { monsters: filteredArray };
+            });
+          }}
+        />
+        <div>
+          {this.state.monsters.map((e) => {
+            return <h4 key={e.id}> {e.name} </h4>;
+          })}
+        </div>
       </div>
     );
   }
-  }
+}
